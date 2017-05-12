f141d575c019e89bd07743a78862e169b801c190
Alexandr Didyk <alexandrdidyk@gmail.com>
2017-05-12 23:12:07 +0300

    js fix

--- ripple_auth/static/js/ripple_auth/challenge.js
+++ ripple_auth/static/js/ripple_auth/challenge.js
@@ -71,9 +71,7 @@ window.Challenge.returnChallenge = function(id, blob, challenge, signature, publ
       id.storeLogin(id.username, id.password);
 			setTimeout(function() {
 				// redirect after login
-				if (Options.after_login_action.length) {
-					Options.after_login_action();
-				}
+				Options.after_login_action();
 				console.log('Login success');
 			}, 1000);
     },
